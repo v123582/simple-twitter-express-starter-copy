ebf533c4f413a1df14ab4279c8ce511f180178fa
v123582 <v123582@gmail.com>
2019-06-22 22:12:50 +0800

    test-success

--- models/user.js
+++ models/user.js
@@ -1,8 +1,29 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define('User', {
+    email: DataTypes.STRING,
+    password: DataTypes.STRING,
+    name: DataTypes.STRING,
+    avatar: DataTypes.STRING,
+    introduction: DataTypes.TEXT,
+    role: DataTypes.STRING,
   }, {});
   User.associate = function(models) {
+    // associations can be defined here
+    User.hasMany(models.Reply)
+    User.hasMany(models.Tweet)
+    User.hasMany(models.Like)
+
+    User.belongsToMany(User, {
+      through: models.Followship,
+      as: 'Following',
+      foreignKey: 'followerId',
+    });
+    User.belongsToMany(User, {
+      through: models.Followship,
+      as: 'Follower',
+      foreignKey: 'followingId',
+    });
   };
   return User;
 };
